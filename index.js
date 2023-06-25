16a390e0f05e5163f9119c1d185759dc046a542a
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-06-25 17:00:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+
+const Input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
